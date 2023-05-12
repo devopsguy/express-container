e7b328975a128f0c383374e2ca9d647c90506aa3
Octavian Helm <octavian.helm@lean-coders.at>
2023-05-12 11:07:47 +0200

    Changed response message.

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Express!')
 })
 
 app.listen(port, () => {
-  console.log(`Hello World app listening on port ${port}`)
+  console.log(`Hello Express app listening on port ${port}`)
 })
